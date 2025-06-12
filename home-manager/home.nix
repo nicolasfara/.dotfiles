54c7ef4bb25984b8a13fe0b1a1588f60887b2c01
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-06-12 21:05:36 +0200

    set theme for on-my-zsh shell

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -126,28 +126,32 @@
     google-chrome.enable = true;
     zsh = {
       enable = true;
+      syntaxHighlighting.enable = true;
+
       oh-my-zsh = {
         enable = true;
+        theme = "af-magic";
         plugins = [
           "git"
           "sudo"
           "docker"
         ];
       };
+      
       autosuggestion = {
         enable = true;
       };
       enableCompletion = true;
-      plugins = [
-        {
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }
-      ];
-      initContent = ''
-        source ~/.p10k.zsh
-      '';
+      # plugins = [
+      #   {
+      #     name = "powerlevel10k";
+      #     src = pkgs.zsh-powerlevel10k;
+      #     file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      #   }
+      # ];
+      # initContent = ''
+      #   source ~/.p10k.zsh
+      # '';
     };
     java = {
       enable = true;
