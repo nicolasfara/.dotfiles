3b4cde82f2f13b036d0d97ec8d9073a42d75667b
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-08-31 18:38:38 +0200

    configure plasma panel widget

--- modules/home-manager/plasma.nix
+++ modules/home-manager/plasma.nix
@@ -1,9 +1,49 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs = {
     plasma = {
       enable = true;
-      # etc.
+      
+      panels = [
+        {
+          location = "bottom";
+          widgets = [
+            {
+              kickoff = {
+                sortAlphabetically = true;
+                # icon = "nix-snowflake-white";
+              };
+            }
+            {
+              iconTasks = {
+                launchers = [
+                  "applications:org.kde.dolphin.desktop"
+                  "applications:org.kde.konsole.desktop"
+                ];
+              };
+            }
+            "org.kde.plasma.marginsseparator"
+            "org.kde.plasma.systemmonitor.cpucore"
+            {
+              systemTray.items = {
+                shown = [
+                  "org.kde.plasma.battery"
+                  "org.kde.plasma.bluetooth"
+                  "org.kde.plasma.networkmanagement"
+                  "org.kde.plasma.volume"
+                ];
+                hidden = [];
+              };
+            }
+            {
+              digitalClock = {
+                calendar.firstDayOfWeek = "monday";
+                time.format = "24h";
+              };
+            }
+          ];
+        }
+      ];
     };
   };
 }
