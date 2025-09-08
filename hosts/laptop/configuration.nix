21fd2e63a119ba209326dc3415d7cd682ff60cae
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-09-08 21:17:23 +0200

    enable virtual camera

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -41,6 +41,14 @@ in
 
   boot.kernelPackages = latestKernelPackage;
 
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+
   # Enable ZFS support
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
