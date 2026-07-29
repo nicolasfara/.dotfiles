57d9f6d2684316111a9bd48394ed2ef9805a0bf6
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2026-07-29 08:53:40 +0200

    remove android device in syncthings

--- modules/home-manager/syncthing.nix
+++ modules/home-manager/syncthing.nix
@@ -19,13 +19,12 @@
       };
       devices = {
         "alice" = { id = "Q3WRVS2-WM635OS-7ZCFL7V-4ZWBUA2-W6PM4C4-DVY43KB-GM5XFH2-JZAZFQL"; };
-        "android" = { id = "XUEC4SS-EGFL3SZ-EREYLUV-GBA7O2Z-EPIU4SO-A7XB4BV-46NYGSS-IUYVLQ4"; };
         "julia" = { id = "OCMKFZ4-XQ3LLXM-G4L3HYA-KOKRMEE-RF34KVT-3QVEBJI-BED73BR-JY624Q5"; };
       };
       folders = {
         "Documents" = {
           path = "${config.home.homeDirectory}/Documents";
-          devices = [ "alice" "android" "julia" ];
+          devices = [ "alice" "julia" ];
           ignorePerms = false;
         };
       };
