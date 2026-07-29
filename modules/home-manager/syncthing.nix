20f5d94451194d1c140ddcf29749d2efc7185771
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2026-07-29 08:43:13 +0200

    ignore folders and files via globs

--- modules/home-manager/syncthing.nix
+++ modules/home-manager/syncthing.nix
@@ -20,12 +20,12 @@
       devices = {
         "alice" = { id = "Q3WRVS2-WM635OS-7ZCFL7V-4ZWBUA2-W6PM4C4-DVY43KB-GM5XFH2-JZAZFQL"; };
         "android" = { id = "XUEC4SS-EGFL3SZ-EREYLUV-GBA7O2Z-EPIU4SO-A7XB4BV-46NYGSS-IUYVLQ4"; };
-        # "julia" = { id = ""; };
+        "julia" = { id = "OCMKFZ4-XQ3LLXM-G4L3HYA-KOKRMEE-RF34KVT-3QVEBJI-BED73BR-JY624Q5"; };
       };
       folders = {
         "Documents" = {
           path = "${config.home.homeDirectory}/Documents";
-          devices = [ "alice" "android" /*"julia"*/ ];
+          devices = [ "alice" "android" "julia" ];
           ignorePerms = false;
         };
       };
@@ -35,10 +35,10 @@
         ".git"
         ".gitignore"
         ".idea"
-        "node_modules"
-        "vendor"
+        "**/node_modules"
+        "**/vendor"
         # Ignore specific folders
-        "repos"
+        "**/repos"
       ];
     };
   };
